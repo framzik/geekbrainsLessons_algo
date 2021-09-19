351f4416f15bfc99ef22a78c3ea492fc604e23e3
framzik <framzik@gmail.com>
2021-09-19 12:51:32 +0300

    hw 4

--- src/ru/khrebtov/lesson4/Main.java
+++ src/ru/khrebtov/lesson4/Main.java
@@ -2,38 +2,48 @@ package ru.khrebtov.lesson4;
 
 public class Main {
     public static void main(String[] args) {
-        MyLinkedList<Integer> mll = new MyLinkedList<>();
-
-        mll.insertFirst(5);
-        mll.insertFirst(15);
-        mll.insertFirst(25);
-        mll.insertFirst(35);
+//        MyLinkedList<Integer> mll = new MyLinkedList<>();
+//
+//        mll.insertFirst(5);
+//        mll.insertFirst(15);
+//        mll.insertFirst(25);
+//        mll.insertFirst(35);
 
 //        System.out.println(mll.removeFirst());
-
-        System.out.println(mll);
-
-        mll.insertLast(2);
-        mll.insertLast(22);
-
-        System.out.println(mll);
-
-        mll.insert(2, 777);
-        System.out.println(mll);
+//
+//        System.out.println(mll);
+//
+//        mll.insertLast(2);
+//        mll.insertLast(22);
+//
+//        System.out.println(mll);
+//
+//        mll.insert(2, 777);
+//        System.out.println(mll);
 //        System.out.println(mll.removeFirst());
 //        System.out.println(mll);
 //        System.out.println(mll.removeLast());
 //        System.out.println(mll);
-
-        mll.insert(2, 111);
-        mll.insert(2, 22);
-        System.out.println(mll);
 //
-        System.out.println(mll.remove(22));
-        System.out.println(mll);
+//        mll.insert(2, 111);
+//        mll.insert(2, 22);
+//        System.out.println(mll);
+//
+//        System.out.println(mll.remove(22));
+//        System.out.println(mll);
+//
+//        for (Integer x : mll) {
+//            System.out.println(x);
+//        }
 
-        for (Integer x : mll) {
-            System.out.println(x);
-        }
+        MyLinkedQueue<Integer> mLQ = new MyLinkedQueue();
+        mLQ.offer(5);
+        mLQ.offer(7);
+        mLQ.offer(8);
+        mLQ.offer(null);
+        System.out.println(mLQ);
+        System.out.println(mLQ.remove());
+        System.out.println(mLQ);
+        System.out.println(mLQ.indexOf(null));
     }
 }
